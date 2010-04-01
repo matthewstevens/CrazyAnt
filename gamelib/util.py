63932be74d2aefc0c5883fdcfc12c2fbcee8770e
Matthew Stevens <matthew.stevens@gmail.com>
2010-04-01 23:22:54 +1100

    Added util.py

---
+++ gamelib/util.py
@@ -0,0 +1,14 @@
+"""
+Helpful stuff that is useful for everyone
+"""
+
+import os
+
+FILE_PATH = os.path.abspath(os.path.dirname(__file__))
+DATA_PATH = os.path.normpath(os.path.join(FILE_PATH, '..', 'data'))
+
+def file_path(file_name):
+	os.path.join(DATA_PATH, file_name)
+
+def load(file_name):
+	return open(file_path(file_name))
